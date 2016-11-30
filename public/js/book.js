9b978c03d2d36bcf4df8a4c576a1ffe9a94d2c88
purple-kisa <hazellau.rcy@gmail.com>
2016-11-30 19:21:39 +0800

    updated book page - add to cart button - submit review button with data required

--- public/js/book.js
+++ public/js/book.js
@@ -3,3 +3,17 @@ $(window).on('load', function () {
     console.log("MEOW");
     $('#rating').barrating({theme:'fontawesome-stars'});
 });
+
+function addToCart() {
+    var isbn = $("#isbn").text().split(" ")[1];
+    var user = $("#user").text();
+    var copies = $("#copies").val();
+    console.log("isbn is " + isbn + ", user is " + user + ", copies is " + copies);
+};
+
+function submitReview() {
+    var review = $("#review_text").val();
+    var score = $("#rating").val();
+    var date = new Date();
+    console.log("review is " + review + ", score is " + score + ", and date is " + date);
+}
